e33eb543a4957d40e7f5211fa7d906183405a64c
Gabriela Zapata <gabyzda@gmail.com>
2022-08-11 09:36:54 -0300

    Reorganizando estilos

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Agenda de Contatos</h1>
-      </header>
+   
     </div>
   );
 }
