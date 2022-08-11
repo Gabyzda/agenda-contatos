6c1dfeade1aeee2b058cf94f2ae53df17e0bdf4e
Gabriela Zapata <gabyzda@gmail.com>
2022-08-11 11:58:43 -0300

    Importar bootstrap css y js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import App from './App';
 /* Fontawesome icons */
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
+/* Bootstrap 5.2 */
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
