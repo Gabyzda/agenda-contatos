5b5ec8b61b53431cf2f2d9fcdf9dc47d707c0bdd
Gabriela Zapata <gabyzda@gmail.com>
2022-08-11 12:13:05 -0300

    Add clase/ícono btn me-2=  bootstrap/ awesome

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
    <h1>App Component</h1>
+   <button className="btn btn-success me-2">
+    <i className="fa fa-home"/>Olá</button>
+   <button className="btn btn-success me-2">Olá</button>
     </div>
   );
 }
