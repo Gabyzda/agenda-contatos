85edb1b146cedb07c28214be90e0d835b876ab4e
Gabriela Zapata <gabyzda@gmail.com>
2022-08-12 01:06:50 -0300

    Uniformar todas funciones a Arrow Function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-   <h1>App Component</h1>
-   {/* <button className="btn btn-success me-2">
-    <i className="fa fa-home"/>Olá</button>
-   <button className="btn btn-success me-2">Olá</button> */}
-    </div>
+    <>
+
+    </>
   );
 }
 
-export default App;
+export { App }
