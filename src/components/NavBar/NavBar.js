9094afc5f7302d553ba9bb4afdf0df670d54bc2a
Gabriela Zapata <gabyzda@gmail.com>
2022-08-13 08:37:07 -0300

    add container y tag de anclaje para nuestra marca import Link

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <>
-            <nav className='navbar navbar-dark bg-dark navbar-expand-sm'>
-
+            <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+                <div className="container">
+                    <Link to={'/'} className="navbar-brand">Contact Gabyzda</Link>
+                </div>
             </nav>
         </>
     )
