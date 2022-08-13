ea4b2ab3abd58e4c5a0135103a09603e3d3179fd
Gabriela Zapata <gabyzda@gmail.com>
2022-08-13 09:00:07 -0300

    Add text-warning en i y marca para color llamativo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
             <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
                 <div className="container">
                     <Link to={'/'} className="navbar-brand">
-                        <i className='fa-solid fa-address-book'/> Contacts Gabyzda</Link>
+                        <i className='fa-solid fa-address-book text-warning'/> Contacts <span className='text-warning'>Gabyzda</span></Link>
                 </div>
             </nav>
         </>
