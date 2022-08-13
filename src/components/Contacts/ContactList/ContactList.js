f26a059fc61a9fb1225ee653274947f337909978
Gabriela Zapata <gabyzda@gmail.com>
2022-08-13 16:40:10 -0300

    Import Link > aplicar btn add NavBar new contact

--- src/components/Contacts/ContactList/ContactList.js
+++ src/components/Contacts/ContactList/ContactList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const ContactList = () => {
     return (
@@ -8,7 +9,9 @@ const ContactList = () => {
                     <div className='grid'>
                         <div className="row">
                             <div className='col'>
-                                
+                                <p className='h3'>Agenda de Contatos
+                                <Link to={'/contacts/add'}></Link>
+                                </p>
                             </div>
                         </div>
 
