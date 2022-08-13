0ee7c98cc5b20cb3e98f08527f4c1f82199068a5
Gabriela Zapata <gabyzda@gmail.com>
2022-08-13 17:24:45 -0300

    Add descripción de mi lista de contactos

--- src/components/Contacts/ContactList/ContactList.js
+++ src/components/Contacts/ContactList/ContactList.js
@@ -12,6 +12,7 @@ const ContactList = () => {
                                 <p className='h3'>Contacts Gabyzda 
                                 <Link to={'/contacts/add'} className='btn btn-success ms-2'><i className='fa fa-square-plus me-2'/>New</Link>
                                 </p>
+                                <p>Bienvenidos a mi Agenda de Contactos para uso exclusivo en TOTI. Aquí encontramos la Turma 16, maravillosos, competentes y bondadosos compañeros, cabe destacar que nuestro Amado Coordinador quien nos acompaña y orienta capazmente se encuentra registrado también. Soy feliz de ser TOTER!!!</p>
                             </div>
                         </div>
 
