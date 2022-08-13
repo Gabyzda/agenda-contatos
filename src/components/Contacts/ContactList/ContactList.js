e49f72f9d5c8052a0134aca82db60e123eff6f17
Gabriela Zapata <gabyzda@gmail.com>
2022-08-13 17:14:25 -0300

    Add margen superior en section de ContactList

--- src/components/Contacts/ContactList/ContactList.js
+++ src/components/Contacts/ContactList/ContactList.js
@@ -4,12 +4,12 @@ import {Link} from "react-router-dom";
 const ContactList = () => {
     return (
         <>
-            <section className='contact-search'>
+            <section className='contact-search p-3'>
                 <div className='container'>
                     <div className='grid'>
                         <div className="row">
                             <div className='col'>
-                                <p className='h3'>Contatos 
+                                <p className='h3'>Contacts Gabyzda 
                                 <Link to={'/contacts/add'} className='btn btn-success ms-2'><i className='fa fa-square-plus me-2'/>New</Link>
                                 </p>
                             </div>
