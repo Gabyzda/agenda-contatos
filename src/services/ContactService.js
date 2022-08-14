cc3b9f2f5325d89b99114931949c95d118b9f696
Gabriela Zapata <gabyzda@gmail.com>
2022-08-14 20:59:35 -0300

    creada ruta para tomar todos los contatos de URL

--- src/services/ContactService.js
+++ src/services/ContactService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export class ContactService {
-    static serverURL = 'http://localhost:9000/contatos';
-    
+    static serverURL = 'http://localhost:9000';
+
+    static getAllContacts(){
+        const dataURL = '${this.serverURL}/contatos';
+    }
 }
