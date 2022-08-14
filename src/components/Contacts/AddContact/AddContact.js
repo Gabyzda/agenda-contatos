e8fd3795fe699787a4341d2747a8f4dcd274a539
Gabriela Zapata <gabyzda@gmail.com>
2022-08-13 22:51:47 -0300

    Creada bienvenida inicial de AddContact

--- src/components/Contacts/AddContact/AddContact.js
+++ src/components/Contacts/AddContact/AddContact.js
@@ -3,7 +3,17 @@ import React from 'react';
 const AddContact = () => {
     return (
         <>
-            <h2>AddContact</h2>
+            <section className='Add-Contact'>
+                <div className='container'>
+                    <div className='row'>
+                        <p className='h3 text-success fw-bold'>Create Contact</p>
+                        <p className='fst-italic'>Podremos adicionar más contactos de TOTI conforme vayamos conociendo nuestro Equipo. Serán Bienvenidos a mi Agenda de Contactos, un placer TOTER!!!</p>
+
+                    </div>
+
+                </div>
+
+            </section>
         </>
     )
 };
