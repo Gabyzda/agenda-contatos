19462c8fcc75e33de068d63245d457274bbaf957
Gabriela Zapata <gabyzda@gmail.com>
2022-08-13 21:33:02 -0300

    Add flex en botones para separar

--- src/components/Contacts/ContactList/ContactList.js
+++ src/components/Contacts/ContactList/ContactList.js
@@ -41,25 +41,25 @@ const ContactList = () => {
                         <div className='col-md-4'>
                             <div className='card'>
                                 <div className='card-body'>
-                                    <div className='row'>
-                                        <div className='col-md-11'>
+                                    <div className='row align-items-center'>
+                                        <div className='col-md-10 d-flex flex-row'>
                                             <ul className='list-group'>
                                                 <li className='list-group-item list-group-item-action'>
-                                                    Name : <span className='fw-bold'>Ankys Serrano</span>
+                                                    Name: <span className='fw-bold'>Ankys Serrano</span>
+                                                </li>
+                                                <li className='list-group-item list-group-item-action d-flex flex-row'>
+                                                    Email: <span className='fw-bold'> serranoankys@gmail.com</span>
                                                 </li>
                                                 <li className='list-group-item list-group-item-action'>
-                                                    Email : <span className='fw-bold'>serranoankys@gmail.com</span>
-                                                </li>
-                                                <li className='list-group-item list-group-item-action'>
-                                                    Mobile : <span className='fw-bold'>42999091697</span>
+                                                    Mobile: <span className='fw-bold'>42999091697</span>
                                                 </li>
                                             </ul>
                                         </div>
-                                        <div className='col-md-1'>
-                                            <Link to={'/contacts/edit/:contactId'} className='btn btn-warning'>
+                                        <div className='col-md-2 d-flex flex-column align-items-center'>
+                                            <Link to={'/contacts/edit/:contactId'} className='btn btn-warning my-1'>
                                                 <i className='fa fa-pen-to-square' />
                                             </Link>
-                                            <button className='btn btn-danger'>
+                                            <button className='btn btn-danger my-1'>
                                                 <i className='fa fa-trash-can' />
                                             </button>
                                         </div>
