3aafec43cc4511ecddf0b3eb632749fd8bbde548
Gabriela Zapata <gabyzda@gmail.com>
2022-08-13 23:03:26 -0300

    Creada estructura container formulario AddContact

--- src/components/Contacts/AddContact/AddContact.js
+++ src/components/Contacts/AddContact/AddContact.js
@@ -6,11 +6,20 @@ const AddContact = () => {
             <section className='add-contact p-3'>
                 <div className='container'>
                     <div className='row'>
-                        <p className='h4 text-success fw-bold'>Create Contact</p>
-                        <p className='fst-italic'>Podremos adicionar más contactos de TOTI conforme vayamos conociendo nuestro Equipo. Serán Bienvenidos a mi Agenda de Contactos, un placer TOTER!!!</p>
+                        <div className='col'>
+                            <p className='h4 text-success fw-bold'>Create Contact</p>
+                            <p className='fst-italic'>Podremos adicionar más contactos de TOTI conforme vayamos conociendo nuestro Equipo. Serán Bienvenidos a mi Agenda de Contactos, un placer TOTER!!!</p>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col-md-4'>
+                            <form>
+                                
+                            </form>
+
+                        </div>
 
                     </div>
-
                 </div>
 
             </section>
