ee83ab1969098739351587aa1fd224e1adc6ecad
Gabriela Zapata <gabyzda@gmail.com>
2022-08-13 22:57:23 -0300

    Añadido margen en saludo AddContact y letra menor

--- src/components/Contacts/AddContact/AddContact.js
+++ src/components/Contacts/AddContact/AddContact.js
@@ -3,10 +3,10 @@ import React from 'react';
 const AddContact = () => {
     return (
         <>
-            <section className='Add-Contact'>
+            <section className='add-contact p-3'>
                 <div className='container'>
                     <div className='row'>
-                        <p className='h3 text-success fw-bold'>Create Contact</p>
+                        <p className='h4 text-success fw-bold'>Create Contact</p>
                         <p className='fst-italic'>Podremos adicionar más contactos de TOTI conforme vayamos conociendo nuestro Equipo. Serán Bienvenidos a mi Agenda de Contactos, un placer TOTER!!!</p>
 
                     </div>
