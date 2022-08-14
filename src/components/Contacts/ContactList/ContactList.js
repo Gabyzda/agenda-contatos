391084acaddf06932d4d89a66cd3baac64f64b33
Gabriela Zapata <gabyzda@gmail.com>
2022-08-14 17:58:39 -0300

    Reorganizado lista contactos principal

--- src/components/Contacts/ContactList/ContactList.js
+++ src/components/Contacts/ContactList/ContactList.js
@@ -35,90 +35,25 @@ const ContactList = () => {
                 </div>
             </section>
 
-            <section className='contact-ist'>
+            <section className='contact-list'>
                 <div className='container'>
                     <div className='row'>
                         <div className='col-md-4'>
                             <div className='card'>
                                 <div className='card-body'>
                                     <div className='row align-items-center d-flex justify-content-around'>
-                                        <div className='col-md-10 d-flex flex-row'>
-                                            <ul className='list-group'>
-                                                <li className='list-group-item list-group-item-action'>
-                                                    Name: <span className='fw-bold'>Ankys Serrano</span>
-                                                </li>
-                                                <li className='list-group-item list-group-item-action d-flex flex-row'>
-                                                    Email: <span className='fw-bold'> serranoankys@gmail.com</span>
-                                                </li>
-                                                <li className='list-group-item list-group-item-action'>
-                                                    Mobile: <span className='fw-bold'>42999091697</span>
-                                                </li>
-                                            </ul>
+                                        <div className='col-md-6 d-flex flex-row align-items-center'>
+                                           <span className='fw-bold'>Ankys Serrano</span>
                                         </div>
-                                        <div className='col-md-2 d-flex flex-column align-items-center'>
-                                            <Link to={'/contacts/edit/:contactId'} className='btn btn-success my-1'>
-                                                <i className='fa fa-user-pen'/>
+                                        <div className='col-md-6 d-flex flex-row align-items-center'>
+                                            <Link to={'/contacts/view/:contactId'} className='btn btn-warning ms-1'>
+                                                <i className='fa fa-address-card' />
                                             </Link>
-                                            <button className='btn btn-danger my-1'>
-                                                <i className='fa fa-trash-can'/>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='col-md-4'>
-                            <div className='card'>
-                                <div className='card-body'>
-                                    <div className='row align-items-center d-flex justify-content-around'>
-                                        <div className='col-md-10 d-flex flex-row'>
-                                            <ul className='list-group'>
-                                                <li className='list-group-item list-group-item-action'>
-                                                    Name: <span className='fw-bold'>Ankys Serrano</span>
-                                                </li>
-                                                <li className='list-group-item list-group-item-action d-flex flex-row'>
-                                                    Email: <span className='fw-bold'> serranoankys@gmail.com</span>
-                                                </li>
-                                                <li className='list-group-item list-group-item-action'>
-                                                    Mobile: <span className='fw-bold'>42999091697</span>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <div className='col-md-2 d-flex flex-column align-items-center'>
-                                            <Link to={'/contacts/edit/:contactId'} className='btn btn-success my-1'>
-                                                <i className='fa fa-user-pen'/>
+                                            <Link to={'/contacts/edit/:contactId'} className='btn btn-success ms-1'>
+                                                <i className='fa fa-user-pen' />
                                             </Link>
-                                            <button className='btn btn-danger my-1'>
-                                                <i className='fa fa-trash-can'/>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='col-md-4'>
-                            <div className='card'>
-                                <div className='card-body'>
-                                    <div className='row align-items-center d-flex justify-content-around'>
-                                        <div className='col-md-10 d-flex flex-row'>
-                                            <ul className='list-group'>
-                                                <li className='list-group-item list-group-item-action'>
-                                                    Name: <span className='fw-bold'>Ankys Serrano</span>
-                                                </li>
-                                                <li className='list-group-item list-group-item-action d-flex flex-row'>
-                                                    Email: <span className='fw-bold'> serranoankys@gmail.com</span>
-                                                </li>
-                                                <li className='list-group-item list-group-item-action'>
-                                                    Mobile: <span className='fw-bold'>42999091697</span>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <div className='col-md-2 d-flex flex-column align-items-center'>
-                                            <Link to={'/contacts/edit/:contactId'} className='btn btn-success my-1'>
-                                                <i className='fa fa-user-pen'/>
-                                            </Link>
-                                            <button className='btn btn-danger my-1'>
-                                                <i className='fa fa-trash-can'/>
+                                            <button className='btn btn-danger ms-1'>
+                                                <i className='fa fa-trash-can' />
                                             </button>
                                         </div>
                                     </div>
