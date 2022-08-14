f2461902f6d110472804cae33a494d09cf4b6c2d
Gabriela Zapata <gabyzda@gmail.com>
2022-08-14 18:31:04 -0300

    Editado componente ViewContact

--- src/components/Contacts/ViewContact/ViewContact.js
+++ src/components/Contacts/ViewContact/ViewContact.js
@@ -4,35 +4,42 @@ import { Link } from 'react-router-dom';
 const ViewContact = () => {
     return (
         <>
-            <div className='col-md-4'>
-                            <div className='card'>
-                                <div className='card-body'>
-                                    <div className='row align-items-center d-flex justify-content-around'>
-                                        <div className='col-md-10 d-flex flex-row'>
-                                            <ul className='list-group'>
-                                                <li className='list-group-item list-group-item-action'>
-                                                    Name: <span className='fw-bold'>Ankys Serrano</span>
-                                                </li>
-                                                <li className='list-group-item list-group-item-action d-flex flex-row'>
-                                                    Email: <span className='fw-bold'> serranoankys@gmail.com</span>
-                                                </li>
-                                                <li className='list-group-item list-group-item-action'>
-                                                    Mobile: <span className='fw-bold'>42999091697</span>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <div className='col-md-2 d-flex flex-column align-items-center'>
-                                            <Link to={'/contacts/edit/:contactId'} className='btn btn-success my-1'>
-                                                <i className='fa fa-user-pen'/>
-                                            </Link>
-                                            <button className='btn btn-danger my-1'>
-                                                <i className='fa fa-trash-can'/>
-                                            </button>
-                                        </div>
+            <section className='add-contact p-3'>
+                <div className='container'>
+                <div className='row'>
+                        <div className='col'>
+                            <p className='h4 text-success fw-bold'>View Contact</p>
+                            <p className='fst-italic'>Podremos ver nuestros contactos de TOTI aquí, Así es el detalle de mi Agenda de Contactos TOTER!!!</p>
+                        </div>
+                    </div>
+                    <div className='col-md-4' >
+                        <div className='card'>
+                            <div className='card-body'>
+                                <div className='row align-items-center d-flex justify-content-around'>
+                                    <div className='col-md-10 d-flex flex-row align-items-center'>
+                                        <ul className='list-group'>
+                                            <li className='list-group-item list-group-item-action'>
+                                                Name: <span className='fw-bold'>Ankys Serrano</span>
+                                            </li>
+                                            <li className='list-group-item list-group-item-action d-flex flex-row'>
+                                                Email: <span className='fw-bold'> serranoankys@gmail.com</span>
+                                            </li>
+                                            <li className='list-group-item list-group-item-action'>
+                                                Mobile: <span className='fw-bold'>42999091697</span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className='col-md-2 d-flex flex-column align-items-center'>
+                                        <Link to={'/contacts/list'} className='btn btn-dark'>
+                                            Back
+                                        </Link>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 };
